6c57ea594facc5ea2e1361f6f78bebe0b016bb78
SangZhong <84898182+SangZhong@users.noreply.github.com>
2022-05-16 23:55:11 +0800

    add rule

---
+++ action.php
@@ -0,0 +1,30 @@
+<?php
+
+// CREATE TABLE `involveasiaassessment`.`RevenueGroup` ( `ID` INT NOT NULL AUTO_INCREMENT , `GroupName` VARCHAR(300) NOT NULL , `GroupDescription` VARCHAR(300) NOT NULL , `isSpecialGroup` BOOLEAN NOT NULL DEFAULT FALSE , PRIMARY KEY (`ID`) ENGINE = InnoDB;
+// CREATE TABLE `involveasiaassessment`.`RevenueRule` ( `ID` INT NOT NULL AUTO_INCREMENT , `Field` VARCHAR(300) NOT NULL , `Operator` VARCHAR(20) NOT NULL , `Amount` INT(3) NOT NULL DEFAULT '0' , `ParentRevenueID` INT(5) NOT NULL , PRIMARY KEY (`ID`)) ENGINE = InnoDB;
+// CREATE TABLE `involveasiaassessment`.`RevenueParameter` ( `ID` INT NOT NULL AUTO_INCREMENT , `ParentRuleID` INT(5) NOT NULL, PRIMARY KEY (`ID`)) ENGINE = InnoDB;
+  
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+    $dbname = "involveasiaassessment";
+    
+    // Create connection
+    $conn = new mysqli($servername, 
+        $username, $password, $dbname);
+    
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " 
+            . $conn->connect_error);
+    }
+    
+    $sqlquery = "INSERT INTO revenuegroup VALUES 
+        ('Test', 'Just to test connection', '0')"
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "record inserted successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+?>
--- plugin.js
+++ plugin.js
@@ -38,21 +38,29 @@ $(document).ready(function(){
             
     });
 
-    // $(".addRuleBtn").click(function() {
-    //     console.log("pass");
-    //     // AddRuleDiv();
-    //     document.getElementById("ruleDiv").insertAdjacentHTML("beforeend", paramBlock);
-    //     // this.manageAddMinusBtn();
-    // });
+    $(".addParamBtn").click(function() {
+        console.log("pass");
+        // AddRuleDiv();
+        document.getElementById("ruleDiv").insertAdjacentHTML("beforeend", paramBlock);
+        // this.manageAddMinusBtn();
+        LoopAddParamBtn();
+    });
 
-    LoopAddParamBtn();
- 
+    //LoopAddParamBtn();
+    //LoopAddRuleBtn();
+
+    $(".addRuleBtn").click(function() {
+        console.log("add rule button pass" );
+        document.getElementById("allRulesDiv").insertAdjacentHTML("beforeend", ruleBlock);
+        LoopAddRuleBtn();
+    })
+
 });
 
 var paramBlock = `
 <div style="display: inline">
     <input type="text" id="parameter" name="parameter" placeholder="Enter parameter" style="width: 30%;"/>
-    <button type="button" class="button-svg addRuleBtn">
+    <button type="button" class="button-svg addParamBtn">
     <img src="./Instructions/add_circle_outline.svg"/>
     </button>
     <br/>
@@ -60,51 +68,81 @@ var paramBlock = `
 `;
 
 var ruleBlock = `
-<div>
-    If 
-    <select id="field" name="field" style="width: 20%;">
-        <option value="afff_sub_1">afff_sub_1</option>
-        <option value="afff_sub_2">afff_sub_2</option>
-        <option value="afff_sub_3">afff_sub_3</option>
-        <option value="afff_sub_4">afff_sub_4</option>
-    </select>
-    <select id="operator" name="operator" style="width: 20%;">
-        <option value="is_not">is not</option>
-        <option value="is">is</option>
-        <option value="starts_with">starts with</option>
-        <option value="ends_with">ends with</option>
-        <option value="contains">contains</option>
-        <option value="does_not_contain">doesn't contain</option>
-    </select>
-    ` + paramBlock + `
+<div id="rule" class="container-inner container-border container-border-dotted">
+    <p>Rule 1</p>
+    <div class="container-grey">
+        <div id="ruleDiv">
+            <div>
+                If 
+                <select id="field" name="field" style="width: 20%;" placeholder="Select field">
+                    <option value="afff_sub_1">afff_sub_1</option>
+                    <option value="afff_sub_2">afff_sub_2</option>
+                    <option value="afff_sub_3">afff_sub_3</option>
+                    <option value="afff_sub_4">afff_sub_4</option>
+                </select>
+                <select id="operator" name="operator" style="width: 20%;" placeholder="Select operator">
+                    <option value="is_not">is not</option>
+                    <option value="is">is</option>
+                    <option value="starts_with">starts with</option>
+                    <option value="ends_with">ends with</option>
+                    <option value="contains">contains</option>
+                    <option value="does_not_contain">doesn't contain</option>
+                </select>
+                ` + paramBlock + `
+            </div>
+        </div>
+    </div>
+    <p>
+        then revenue is 
+        <input type="text" id="revenue" name="revenue" placeholder="Enter amount"/>
+    </p>
 </div>
+
 `;
 
-function AddRuleDiv() {
-    console.log("sdfaf");
-    document.getElementById("ruleDiv").insertAdjacentHTML("afterend", ruleBlock);
+var index = 0;
+
+function AddRuleDiv(index) {
+    if (index == undefined) {
+        index = "0";
+    }
+    console.log("11111");
+    // ruleBlock = ruleBlock.replace("rule", "rule" + index);
+    document.getElementById("allRulesDiv").insertAdjacentHTML("afterend", ruleBlock);
     // this.manageAddMinusBtn();
 }
 
+function LoopAddRuleBtn() {
+    console.log("sss");
+    var allAddRuleButtons = document.querySelectorAll("[class*=addRuleBtn]");
+    console.log("Found", allAddRuleButtons.length, "div which class starts with “button”.");
+    for (var i = 0; i < allAddRuleButtons.length; i++) {
+        allAddRuleButtons[i].addEventListener("click", function() {
+            AddRuleDiv(i.toString());
+        });
+    }
+    LoopAddParamBtn();
+}
+
 function AddParamDiv() {
-    console.log("pass");
+    console.log("pass lower");
     document.getElementById("ruleDiv").insertAdjacentHTML("beforeend", paramBlock);
     LoopAddParamBtn();
 }
 
 function LoopAddParamBtn() {
-    var allAddParamButtons = document.querySelectorAll('[class*=addRuleBtn]');
+    var allAddParamButtons = document.querySelectorAll("[class*=addParamBtn]");
     console.log("Found", allAddParamButtons.length, "div which class starts with “button”.");
 
     for (var i = 0; i < allAddParamButtons.length; i++) {
-        allAddParamButtons[i].addEventListener('click', function() {
-            AddParamDiv();
+        allAddParamButtons[i].addEventListener("click", function() {
+            AddParamDiv(2);
         });
     }
 }
 
 function manageAddMinusBtn() {
-    if ( $(this).is($(".addRuleBtn:last")) ) 
+    if ( $(this).is($(".addParamBtn:last")) ) 
     {
     // I'm the last check element!
     console.log("last checked");
